e476d7ab91562ecc741f724c1827d28eb116f730
Jenn Greiner <jenn.greiner1@gmail.com>
2021-02-07 20:51:34 -0700

    css styling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,9 @@
 
-var searchBtn = document.getElementById('search-button');
-var cityName = 'santa fe';
+var searchEnter = document.getElementById("searchEnter");
+// console.log(searchEnter);
+var cityName = 'frederick';
 var searchList = document.querySelector('ul');
-// var cityName = document.getElementById('city-search');
+// var cityName = document.getElementById('input');
 // console.log(cityName);
 var cityURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=49007e1734cfd0ecf0b4c3ad67f3f238';
 var todaysDate = '';
@@ -15,14 +16,12 @@ var currentWindSpeed = '';
 
 // page opens to last searched city
 getWeather(cityName);
-// get local storage when page loads
 
 // fetch from openweather API
 function getWeather(cityName) {
   fetch(cityURL)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
       // save lat and long variables
 
       var lat = data.city.coord.lat;
@@ -30,8 +29,6 @@ function getWeather(cityName) {
       fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=49007e1734cfd0ecf0b4c3ad67f3f238')
         .then(response => response.json())
         .then(uvData => {
-          console.log(uvData);
-          console.log(uvData.daily[0].uvi);
           if (data) {
             displayCurrent(data, uvData);
             displayForecast(data);
@@ -40,12 +37,9 @@ function getWeather(cityName) {
     })
 }
 // getCity()
-
-
 function displayCurrent(weatherData, uvData) {
   //City Name
   var cityName = weatherData.city.name;
-  console.log(cityName);
   // todays date
   var todaysDate = moment().format('MM/DD/YYYY')
   document.getElementById('city-name').textContent = cityName + ' (' + todaysDate + ')'
@@ -63,7 +57,6 @@ function displayCurrent(weatherData, uvData) {
   document.getElementById('wind-speed').textContent = currentWindSpeed + ' MPH';
   // uv index
   var uvIndex = uvData.daily[0].uvi;
-  console.log('uvi value: ' + uvIndex);
   if (uvIndex <= 2.99) {
     $('#uv-index').addClass('low')
   }
@@ -75,7 +68,6 @@ function displayCurrent(weatherData, uvData) {
   };
   document.getElementById('uv-index').textContent = uvIndex;
 }
-
 // display 5 day forecast
 function displayForecast(weatherData) {
 
@@ -83,52 +75,52 @@ function displayForecast(weatherData) {
   var forecastDate1 = moment().add(1, 'days').format('MM/DD/YYYY')
   document.getElementById('forecast-date').textContent = forecastDate1
   // Icon
-  var iconURL1 = 'https://openweathermap.org/img/wn/' + weatherData.list[8].weather[0].icon + '.png';
+  var iconURL1 = 'https://openweathermap.org/img/wn/' + weatherData.list[7].weather[0].icon + '.png';
   document.getElementById('forecast-icon').src = iconURL1
   // Temperature
-  var currentTemp1 = weatherData.list[8].main.temp;
+  var currentTemp1 = weatherData.list[7].main.temp;
   document.getElementById('forecast-temp').textContent = currentTemp1 + ' ºF';
   // Humidity
-  var currentHumidity1 = weatherData.list[8].main.humidity;
+  var currentHumidity1 = weatherData.list[7].main.humidity;
   document.getElementById('forecast-humidity').textContent = currentHumidity1 + '%';
 
   //day 2
   var forecastDate2 = moment().add(2, 'days').format('MM/DD/YYYY')
   document.getElementById('forecast-date2').textContent = forecastDate2
   // Icon
-  var iconURL2 = 'https://openweathermap.org/img/wn/' + weatherData.list[16].weather[0].icon + '.png';
+  var iconURL2 = 'https://openweathermap.org/img/wn/' + weatherData.list[14].weather[0].icon + '.png';
   document.getElementById('forecast-icon2').src = iconURL2
   // Temperature
-  var currentTemp2 = weatherData.list[16].main.temp;
+  var currentTemp2 = weatherData.list[14].main.temp;
   document.getElementById('forecast-temp2').textContent = currentTemp2 + ' ºF';
   // Humidity
-  var currentHumidity2 = weatherData.list[16].main.humidity;
+  var currentHumidity2 = weatherData.list[14].main.humidity;
   document.getElementById('forecast-humidity2').textContent = currentHumidity2 + '%';
 
   //day 3
   var forecastDate3 = moment().add(3, 'days').format('MM/DD/YYYY')
   document.getElementById('forecast-date3').textContent = forecastDate3
   // Icon
-  var iconURL3 = 'https://openweathermap.org/img/wn/' + weatherData.list[24].weather[0].icon + '.png';
+  var iconURL3 = 'https://openweathermap.org/img/wn/' + weatherData.list[22].weather[0].icon + '.png';
   document.getElementById('forecast-icon3').src = iconURL3
   // Temperature
-  var currentTemp3 = weatherData.list[24].main.temp;
+  var currentTemp3 = weatherData.list[22].main.temp;
   document.getElementById('forecast-temp3').textContent = currentTemp3 + ' ºF';
   // Humidity
-  var currentHumidity3 = weatherData.list[24].main.humidity;
+  var currentHumidity3 = weatherData.list[22].main.humidity;
   document.getElementById('forecast-humidity3').textContent = currentHumidity3 + '%';
 
   //day 4
   var forecastDate4 = moment().add(4, 'days').format('MM/DD/YYYY')
   document.getElementById('forecast-date4').textContent = forecastDate4
   // Icon
-  var iconURL4 = 'https://openweathermap.org/img/wn/' + weatherData.list[36].weather[0].icon + '.png';
+  var iconURL4 = 'https://openweathermap.org/img/wn/' + weatherData.list[34].weather[0].icon + '.png';
   document.getElementById('forecast-icon4').src = iconURL4
   // Temperature
-  var currentTemp4 = weatherData.list[36].main.temp;
+  var currentTemp4 = weatherData.list[34].main.temp;
   document.getElementById('forecast-temp4').textContent = currentTemp4 + ' ºF';
   // Humidity
-  var currentHumidity4 = weatherData.list[36].main.humidity;
+  var currentHumidity4 = weatherData.list[34].main.humidity;
   document.getElementById('forecast-humidity4').textContent = currentHumidity4 + '%';
 
   //day 5
@@ -146,11 +138,12 @@ function displayForecast(weatherData) {
 
 }
 
-// function startSearch() {
-//   searchHistory.push(cityName.value)
-//   localStorage.setItem('Search History', JSON.stringify(searchHistory));
-//   getWeather(cityName.value);
+// function citySearch() {
+//   var citySearchValue = document.createElement("li");
+//   citySearchValue.textContent = cityName.toUpperCase();
+//   citySearchValue.classList.add(cityList);
 // }
+
 
 // function renderCityList() {
 //   // get info from local storage when the page loads
@@ -158,48 +151,14 @@ function displayForecast(weatherData) {
 //     var newCity = document.createElement('li');
 //     newCity.textContent = searchHistory[i];
 //     console.log(newCity.textContent);
-//     newCity.classList.add('cities');
 //     searchList.prepend(newCity);
 //     console.log(newCity);
 //     // add new city names to the top of the list when search button is clicked
+//     searchHistory.push(cityName.value)
+//     localStorage.setItem('Search History', JSON.stringify(searchHistory));
+//     getWeather(cityName.value);
 //   }
 // }
-
-// function getLocalStorage() {
-//   var searchHistory = JSON.parse(window.localStorage.getItem('Search History')) || [];
-//   if (searchHistory) {
-//     renderCityList()
-//   }
-// }
-// get localstorage to check if city already exists when you save a new city
-// if city doesnt exist, set it to localstorage, then get localstorage again to display on the page. 
-// saveBtn.on('click', function () {
-//   // var time = $(this).parent().attr('id');
-//   // var value = $(this).siblings('.task').val();
-//   // localStorage.setItem(time, value)
-//   var localTimeEvents = {},
-//     lsTimeEvents;
-//   if (localStorage.getItem('timeEvents')) {
-//     lsTimeEvents = JSON.parse(localStorage.getItem('timeEvents'));
-//     lsTimeEvents[$(this).parent().attr('id')] = $(this).siblings('.task').val();
-//     localStorage.setItem('timeEvents', JSON.stringify(lsTimeEvents));
-//   } else {
-//     localTimeEvents[$(this).parent().attr('id')] = $(this).siblings('.task').val();
-//     localStorage.setItem('timeEvents', JSON.stringify(localTimeEvents));
-//   }
-
-// })
-// // get localstorage, look for the object. 
-// // if there is not object, create a new one
-// // if there is an object, JSONparse it to turn it back into an object
-// var displayTimeEvents;
-// if (localStorage.getItem('timeEvents')) {
-//   displayTimeEvents = JSON.parse(localStorage.getItem('timeEvents'));
-//   for (x in displayTimeEvents) {
-//     $('#' + x).find('.task').val(displayTimeEvents[x]);
-//   }
-// }
-
 // function getCity() {
 //   var cityList = document.querySelector('ul');
 //   var listItem = document.createElement('li');
@@ -208,28 +167,19 @@ function displayForecast(weatherData) {
 //   cityList.appendChild(listItem);
 //   console.log(cityURL);
 // }
+
+// function getLocalStorage() {
+//   var searchHistory = JSON.parse(window.localStorage.getItem('Search History')) || [];
+//   if (searchHistory) {
+//     renderCityList()
+//   }
+// }
 // searchBtn.on('click', getCity())
-// search button searches for input
-// dynamically take user input and provide it as a parameter in the open weather api call
-// city = $('#search-input').val();
-// searchBtn.on('click', function () {
-//   console.log(city);
-// });
-// add search to local storage
 
-// local storage persists reload
-
-// click on a city in the search history to view current and future conditions for that city
-
-// display city name, the date, an icon representation of weather conditions, 
+// get localstorage to check if city already exists when you save a new city
+// if city doesnt exist, set it to localstorage, then get localstorage again to display on the page. 
+// get localstorage, look for the object. 
+// if there is not object, create a new one
+// // if there is an object, JSONparse it to turn it back into an object
 
 
-//   // create new div for each stat
-
-    // The UV index color  indicates whether the conditions are favorable, moderate, or severe
-        // if (uv > x) { addClass(high)} else if uv < x && uv > y { addClass(mid)} else {addClass(low)}
-
-// 5 day forcast
-// fetch from openweather api
-
-    // display date, icon, temperature, and humidity
